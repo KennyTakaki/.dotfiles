b01d1e1a835268d0c8f5babd80718cafcf2bdda5
KennyTakaki <kta.it.pc@gmail.com>
2020-07-21 21:22:01 -0700

    python3-venv がpython のlsp に必要らしいので、コメントでinstallに追加。
    python3-venv がpython のlsp に必要らしいので、コメントでinstallに追加。

--- install.sh
+++ install.sh
@@ -14,6 +14,8 @@ if [ $? -eq 1 ]; then
    sudo apt install -y curl
 fi
 
+#sudo apt install -y python3-venv
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 for f in .??*
