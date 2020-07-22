7d06124a7144b3baff4c2db2fff2483bc64622b0
KennyTakaki <kta.it.pc@gmail.com>
2020-07-21 21:36:28 -0700

    python lsp 用に dev-tool 関連のインストールを追加
    python lsp 用に dev-tool 関連のインストールを追加

--- install.sh
+++ install.sh
@@ -14,7 +14,9 @@ if [ $? -eq 1 ]; then
    sudo apt install -y curl
 fi
 
+#these two line needed for python lsp
 #sudo apt install -y python3-venv
+#sudo apt install -y python3-dev build-essential libssl-dev libffi-dev
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
